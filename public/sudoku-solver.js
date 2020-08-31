5571e693c27162f976c17d0cf036e2dc8f7deb68
Marko <michinaux.marko@gmail.com>
2020-08-31 18:25:55 -0400

    finished app, tests still in development

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -1,18 +1,278 @@
 const textArea = document.getElementById('text-input');
-// import { puzzlesAndSolutions } from './puzzle-strings.js';
+const rows = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7,
+    'I': 8
+};
+//Most Puzzle value updates will use the globalPuzzleArr for consistency between the textArea and grid.
+let globalPuzzleArr, twoDimArr;
+
+import {
+    puzzlesAndSolutions
+}
+from './puzzle-strings.js';
+
+const sudokuSolver = () => {
+
+    //The input string goes through a regex pattern treatment and has everything (except the desired inputs) replaced with a dot.
+    const checkLegitInput = (input) => {
+        let validityRegex = /[^1-9.]/;
+
+        return input.toString().replace(validityRegex, '.');
+    }
+
+    //Another string treatment. If empty, returns a string of 81 dots.
+    const getArrayFromString = str => str.length ? checkLegitInput(str).split('') : '.'.repeat(81).split('');
+
+    //Core grid update function.
+    const drawOnCells = arr => {
+        const cells = document.querySelectorAll('.sudoku-input');
+        let i = 0;
+
+        for (const cell of cells) {
+            let num = arr[i];
+
+            num !== '.' ? cell.value = num : cell.value = '';
+            i++;
+        }
+    }
+
+    //This expression gets actual puzzle value index from grid cell position: F(x)= n*10+x-(n+1). For n=row and x=col.
+    //Both argument values have to be typeof 'number'.
+    const calculateIndex = (row, col) => (row * 10) + col - (row + 1);
+
+    // Any puzzle length different than 81 charactes will not trigger a DOM change for grid and global.
+
+    const handleTextArea = function () {
+        this.value = checkLegitInput(this.value);
+
+        if (this.value.length !== 81) {
+            return;
+        }
+        // Not implemented due to meet the requirements of 9th challenge user's story.
+        // Anything more than equal to 81 will get sliced then trigger a DOM change.
+        /*else if (this.value.length > 81)
+        this.value = this.value.split('').slice(0, 81).join('');*/
+        globalPuzzleArr = this.value.split('');
+        drawOnCells(globalPuzzleArr);
+    }
+
+    const handleGridInput = function () {
+        let checkValue = this.value?checkLegitInput(this.value):'';
+
+        /*if (checkValue === '.') {
+        return this.value = '';
+        }*/
+        this.value = checkValue === '.' ? '' : checkValue;
+        // To determine proper elem position on puzzle string. Letters are rows value and num represents column value
+        const[letter, num] = this.id.split('');
+        const idx = calculateIndex(rows[letter], parseInt(num));
+
+        globalPuzzleArr[idx] = checkValue;
+        textArea.value = globalPuzzleArr.join('');
+    }
+
+    const handleClear = () => {
+
+        textArea.value = '';
+        globalPuzzleArr = getArrayFromString(textArea.value);
+        drawOnCells(globalPuzzleArr);
+    }
+
+    // A 9x9 2D Array is needed in order to the algorythm find a possible solution using backtrack method.
+    const createTwoDimensionalArray = str => {
+        /*let grid = [[], [], [], [], [], [], [], [], []],
+        init = 0;
+        const pushValues = (i, arr) => arr.forEach(value => grid[i].push(value));
+
+        for (let i = 0; i < 9; i++) {
+        init = i ? (!(i % 3) ? i * 9 : init + 3) : 0;
+
+        let aux = init;
+        for (let j = 0; j < 3; j++) {
+        let getRowOfNum = str.substring(aux, aux + 3).split('');
+        pushValues(i, getRowOfNum);
+        aux += 9;
+        }
+        }
+
+        return grid;*/
+        let arr = [];
+
+        for (let i = 0; i < Object.keys(rows).length; i++) {
+            let nineLenStr = str.substring(i * 9, i * 9 + 9);
+            arr.push(nineLenStr.split(''));
+        }
+        return arr;
+    }
+
+    const checkRow = (arr, col, num) => {
+        let idx = arr.indexOf(num);
+
+        if (idx === -1 || idx === col) {
+            return false;
+        }
+        return true;
+    }
+
+    const checkCol = (arr, row, col, num) => {
+        for (let i = 0; i < 9; i++) {
+            if (arr[i][col] == num && i != row) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkBox = (arr, row, rowOff, col, colOff, num) => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (
+                    arr[rowOff + i][colOff + j] == num &&
+                    rowOff + i != row &&
+                    colOff + j != col) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    const checkValid = (row, col, num) => {
+        const foundInRow = checkRow(twoDimArr[row], col, num);
+        const foundInCol = checkCol(twoDimArr, row, col, num);
+        const foundInBox = checkBox(twoDimArr, row, row - (row % 3), col, col - (col % 3), num);
+
+        return (!foundInRow && !foundInCol && !foundInBox) ? true : false;
+    };
+
+    const checkSolvable = () => {
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (twoDimArr[i][j] === '.') {
+                    continue
+                }
+                if (!checkValid(i, j, twoDimArr[i][j])) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    const findEmpty = () => {
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (twoDimArr[i][j] === '.') {
+                    return [i, j];
+                }
+            }
+        }
+        return false;
+    }
+
+    // Sudoku Backtracking-7 method explained on geeksforgeeks.org/sudoku-backtracking-7.
+    // Makes sure of safe number before assignment then perform a recursive call until there are not free cells anymore.
+    const findSolution = () => {
+        const foundEmpty = findEmpty(twoDimArr);
+        if (!foundEmpty)
+            return true;
+
+        const[row, col] = foundEmpty;
+        for (let num = 1; num < 10; num++) {
+
+            if (checkValid(row, col, num.toString())) {
+                twoDimArr[row][col] = num.toString();
+                if (findSolution()) {
+                    return true;
+                }
+                // reset value and backtrack
+                twoDimArr[row][col] = '.';
+            }
+
+        }
+        return false;
+    }
+
+    const displayMsg = (elem)=> (visible, msg) => {
+        elem.style.visibility = visible;
+        elem.innerText = msg;
+    }
+    
+    const solvePuzzle = () => {
+        const {
+            value
+        } = textArea;
+        const errorElem = displayMsg(document.getElementById('error-msg'));
+        
+        if (value.length !== 81) {
+
+            errorElem('visible', 'Error: Expected puzzle to be 81 characters long.');
+            return;
+        }
+        twoDimArr = createTwoDimensionalArray(value);
+        if (!checkSolvable()) {
+            errorElem('visible', 'Error: No solution.');
+            return;
+        }
+        if (findSolution()) {
+            errorElem('hidden', '');
+            globalPuzzleArr = [].concat(...twoDimArr);
+            textArea.value = globalPuzzleArr.join('');
+            drawOnCells(globalPuzzleArr);
+            return;
+        }
+        return displayMsg('visible', 'Error:No solution found');
+    }
+
+    return {
+        checkLegitInput,
+        getArrayFromString,
+        drawOnCells,
+        handleTextArea,
+        handleGridInput,
+        handleClear,
+        createTwoDimensionalArray,
+        solvePuzzle
+    }
+
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Load a simple puzzle into the text area
-  textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    let puzzlePicked = Math.floor(Math.random() * 5);
+    // Load a random puzzle into the text area
+    textArea.value = puzzlesAndSolutions[puzzlePicked][0];
+    //textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    const sudoku = sudokuSolver();
+    globalPuzzleArr = sudoku.getArrayFromString(textArea.value);
+    sudoku.drawOnCells(globalPuzzleArr);
+
+    // Add event listener to all the 81 input fields on the grid
+    let gridElems = document.getElementsByClassName('sudoku-input');
+    Array.from(gridElems).forEach((ele) =>
+        ele.addEventListener('input', sudoku.handleGridInput));
+    // Add event listener to textArea, clear-button
+    document.getElementById('clear-button').addEventListener('click', sudoku.handleClear);
+    document.getElementById('solve-button').addEventListener('click', sudoku.solvePuzzle);
+    textArea.addEventListener('input', sudoku.handleTextArea);
 });
 
-/* 
-  Export your functions for testing in Node.
-  Note: The `try` block is to prevent errors on
-  the client side
-*/
+/*
+Export your functions for testing in Node.
+Note: The `try` block is to prevent errors on
+the client side
+ */
 try {
-  module.exports = {
-
-  }
+    module.exports = {
+        checkLegitInput: sudokuSolver().checkLegitInput,
+        solvePuzzle: sudokuSolver().solvePuzzle,
+        createTwoDimensionalArray:sudokuSolver().createTwoDimensionalArray,
+        displayMsg: sudokuSolver().displayMsg
+    }
 } catch (e) {}
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -10,82 +10,211 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-let Solver;
+const {
+    JSDOM
+} = jsdom;
+let Solver, domEvent;
 
 suite('UnitTests', () => {
-  suiteSetup(() => {
-    // Mock the DOM for testing and load Solver
-    return JSDOM.fromFile('./views/index.html')
-      .then((dom) => {
-        global.window = dom.window;
-        global.document = dom.window.document;
-
-        Solver = require('../public/sudoku-solver.js');
-      });
-  });
-  
-  // Only the digits 1-9 are accepted
-  // as valid input for the puzzle grid
-  suite('Function ____()', () => {
-    test('Valid "1-9" characters', (done) => {
-      const input = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-      // done();
+    suiteSetup(function () {
+        // Mock the DOM for testing and load Solver
+        return JSDOM.fromFile('./views/index.html', {
+            runScripts: 'dangerously',
+            resources: 'usable'
+        })
+        .then(dom =>
+            new Promise(resolve => {
+                dom.window.document.addEventListener('DOMContentLoaded', () => {
+                    global.window = dom.window;
+                    global.document = dom.window.document,
+                    domEvent = new window.Event('input', {
+                        bubbles: true,
+                        cancelable: true
+                    });
+                    Solver = require('../public/sudoku-solver');
+                    resolve();
+                })
+            }))
     });
-
-    // Invalid characters or numbers are not accepted 
+    // Only the digits 1-9 are accepted
     // as valid input for the puzzle grid
-    test('Invalid characters (anything other than "1-9") are not accepted', (done) => {
-      const input = ['!', 'a', '/', '+', '-', '0', '10', 0, '.'];
+    suite('Function checkLegitInput(input)', () => {
+        test('Valid "1-9" characters', (done) => {
+            const input = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+            input.forEach((el, i) => {
+                assert.strictEqual(Solver.checkLegitInput(el), input[i]);
+            });
+            done();
+        });
 
-      // done();
-    });
-  });
-  
-  suite('Function ____()', () => {
-    test('Parses a valid puzzle string into an object', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
-      // done();
-    });
-    
-    // Puzzles that are not 81 numbers/periods long show the message 
-    // "Error: Expected puzzle to be 81 characters long." in the
-    // `div` with the id "error-msg"
-    test('Shows an error for puzzles that are not 81 numbers long', done => {
-      const shortStr = '83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const longStr = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...';
-      const errorMsg = 'Error: Expected puzzle to be 81 characters long.';
-      const errorDiv = document.getElementById('error-msg');
-      
-      // done();
-    });
-  });
-
-  suite('Function ____()', () => {
-    // Valid complete puzzles pass
-    test('Valid puzzles pass', done => {
-      const input = '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
-
-      // done();
+        // Invalid characters or numbers are not accepted
+        // as valid input for the puzzle grid
+        test('Invalid characters (anything other than "1-9") are not accepted', (done) => {
+            const input = ['!', 'a', '/', '+', '-', '0', 'v', 0, '.'];
+            input.forEach((el, i) => {
+                assert.strictEqual(Solver.checkLegitInput(el), ".");
+            });
+            done();
+        });
     });
 
-    // Invalid complete puzzles fail
-    test('Invalid puzzles fail', done => {
-      const input = '779235418851496372432178956174569283395842761628713549283657194516924837947381625';
+    suite('Function solvePuzzle() and createTwoDimensionalArray(input)', () => {
+        test('Parses a valid puzzle string into an object', done => {
+            const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+            const output = [
+                [
+                    ".",
+                    ".",
+                    "9",
+                    ".",
+                    ".",
+                    "5",
+                    ".",
+                    "1",
+                    "."
+                ],
+                [
+                    "8",
+                    "5",
+                    ".",
+                    "4",
+                    ".",
+                    ".",
+                    ".",
+                    ".",
+                    "2"
+                ],
+                [
+                    "4",
+                    "3",
+                    "2",
+                    ".",
+                    ".",
+                    ".",
+                    ".",
+                    ".",
+                    "."
+                ],
+                [
+                    "1",
+                    ".",
+                    ".",
+                    ".",
+                    "6",
+                    "9",
+                    ".",
+                    "8",
+                    "3"
+                ],
+                [
+                    ".",
+                    "9",
+                    ".",
+                    ".",
+                    ".",
+                    ".",
+                    ".",
+                    "6",
+                    "."
+                ],
+                [
+                    "6",
+                    "2",
+                    ".",
+                    "7",
+                    "1",
+                    ".",
+                    ".",
+                    ".",
+                    "9"
+                ],
+                [
+                    ".",
+                    ".",
+                    ".",
+                    ".",
+                    ".",
+                    ".",
+                    "1",
+                    "9",
+                    "4"
+                ],
+                [
+                    "5",
+                    ".",
+                    ".",
+                    ".",
+                    ".",
+                    "4",
+                    ".",
+                    "3",
+                    "7"
+                ],
+                [
+                    ".",
+                    "4",
+                    ".",
+                    "3",
+                    ".",
+                    ".",
+                    "6",
+                    ".",
+                    "."
+                ]
+            ];
 
-      // done();
+            assert.deepStrictEqual(Solver.createTwoDimensionalArray(input), output);
+            done();
+        });
+
+        // Puzzles that are not 81 numbers/periods long show the message
+        // "Error: Expected puzzle to be 81 characters long." in the
+        // `div` with the id "error-msg"
+        test('Shows an error for puzzles that are not 81 numbers long', done => {
+            const shortStr = '83.9.....6.62.71...9......1945....4.37.4.3..6..';
+            const longStr = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...';
+            const errorMsg = 'Error: Expected puzzle to be 81 characters long.';
+            const errorDiv = document.getElementById('error-msg');
+            const textArea = document.getElementById('text-input');
+            Solver.displayMsg(errorDiv)('hidden', '');
+            textArea.value = shortStr;
+            //Trigger click event
+            textArea.dispatchEvent(domEvent);
+            document.getElementById('solve-button').click();
+            //assert.equal(errorDiv.innerText, errorMsg);
+            assert.equal(errorDiv.style.visibility, 'visible');
+            // Change back to long string and the error should remain. Start by resetting the value of errorDiv to confirm a positive result
+            /*errorDiv.innerText = '';
+            textArea.value = longStr;
+            textArea.dispatchEvent(domEvent);
+            document.getElementById('solve-button').click();
+            assert.equal(errorDiv.innerText, errorMsg);*/
+            done();
+        });
     });
-  });
-  
-  
-  suite('Function ____()', () => {
-    // Returns the expected solution for a valid, incomplete puzzle
-    test('Returns the expected solution for an incomplete puzzle', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
-      // done();
+
+    suite('Function ____()', () => {
+        // Valid complete puzzles pass
+        test('Valid puzzles pass', done => {
+            const input = '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
+
+            // done();
+        });
+
+        // Invalid complete puzzles fail
+        test('Invalid puzzles fail', done => {
+            const input = '779235418851496372432178956174569283395842761628713549283657194516924837947381625';
+
+            // done();
+        });
     });
-  });
+
+    suite('Function ____()', () => {
+        // Returns the expected solution for a valid, incomplete puzzle
+        test('Returns the expected solution for an incomplete puzzle', done => {
+            const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+
+            // done();
+        });
+    });
 });
